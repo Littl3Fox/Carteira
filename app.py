ca36660508d1c6cc5152d48d44ad75e97cad082e
Litt3Fox <artur.computacional@gmail.com>
2024-02-19 19:23:48 -0300

    Criação de algumas pastas e do layout

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Hello world")
+import os
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import date
+
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+Session(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
