d91082d4429239315e4541deb02694c1e3085da1
Litt3Fox <artur.computacional@gmail.com>
2024-02-20 18:51:13 -0300

    Foi feito o BD, arquivo para mandar notas em pdf, atualizações nos templates

--- app.py
+++ app.py
@@ -4,18 +4,29 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import date
+import tabula
 
 
 app = Flask(__name__)
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    
 Session(app)
 
-@app.route("/")
+db = SQL("sqlite:///users.db")
+
+@app.route("/index")
 def index():
+    '''
+        Aqui eu vou desenvolver uma tabela onde vai aparecer os ativos do usuario.
+        Vai ter os nome do ativo, simbolo, preco_medio, valor total investido, preco atual, e se está tendo um ganho ou perda.
+    '''
     return render_template("index.html")
+
+
+@app.route("/notas", methods=["GET", "POST"])
+def quote():
+    if request.method == "POST":
+        return redirect("/")
+    else:
+        return render_template("notas.html")
